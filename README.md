a237d09f73e8af47537e77ab970920bd06590c57
xshoji <40511798+xshoji@users.noreply.github.com>
2018-07-03 00:19:08 +0900

    update

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
  - [Downloads - The Go Programming Language](https://golang.org/dl/)
  - [Go with Visual Studio Code](https://code.visualstudio.com/docs/languages/go)
  - [Go と Travis CI を連携したり Golint を実行してみたり - kakakakakku blog](https://kakakakakku.hatenablog.com/entry/2015/12/25/233540)
+
+ ### VSCodeでメソッド補完等が効かない
+
+  - [Golangで自分自身で定義したパッケージをインポートする方法あれこれ - Qiita](https://qiita.com/shopetan/items/eddcacec21cc7ea274f9)
+
+  この記事にならって、`~/go/src/[username]/[repository]`配下にcloneしてきて、
+  パッケージの参照を`"github.com/xshoji/go-sample-box/go-first-sample/structs"`とかにしたらうまくいった
--- go-first-sample/main.go
+++ go-first-sample/main.go
@@ -3,24 +3,36 @@ package main
 import (
 	"fmt"
 
-	"./structs"
-	"./utils"
+	"github.com/xshoji/go-sample-box/go-first-sample/structs"
+	"github.com/xshoji/go-sample-box/go-first-sample/utils"
 )
 
 func main() {
+	// primitive types
 	valueInt := 100
 	valueDouble := 100.12
 	valueRune := '\n'
 	valueString := "test"
 	fmt.Printf("%v, %v, %v, %v\n", valueInt, valueDouble, valueRune, valueString)
 
+	// array
 	valueIntArray := [...]int{1, 2, 3}
 	fmt.Printf("%v\n", valueIntArray)
 
-	// メソッドを呼ぶ場合はパッケージ名を先頭につけて呼び出す
+	// slice
+	valueStringSlice := []string{"aaa", "bbb", "ccc"}
+	fmt.Printf("%v\n", valueStringSlice)
+	valueStringSlice = append(valueStringSlice, "ddd")
+	fmt.Printf("%v\n", valueStringSlice)
+
+	for _, v := range valueStringSlice {
+		fmt.Println(v)
+	}
+
+	// public method
+	// Package prefix is required when calling method
 	fmt.Printf("%v\n", utils.JoinString("aaa", "bbb"))
 
-	// 複数の戻り値はそのまま引数として使うことも可能
 	fmt.Printf("%v\n", utils.JoinString(utils.GetMultiReturns()))
 
 	// 構造体の初期化
