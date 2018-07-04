6d5da8cfcf33502aae59b2b0f8c4f8e8745bccc1
xshoji <40511798+xshoji@users.noreply.github.com>
2018-07-05 00:14:01 +0900

    update

--- net/client/client.go
+++ net/client/client.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -36,4 +37,26 @@ func (c *Client) GetAsString() string {
 	body, err := ioutil.ReadAll(resp.Body)
 	bodyString := string(body)
 	return bodyString
+}
+
+// - [golang は ゆるふわに JSON を扱えまぁす! — KaoriYa](https://www.kaoriya.net/blog/2016/06/25/)
+// GetAsObject GetAsObject
+func (c *Client) GetAsObject() interface{} {
+	if c.Url == "" {
+		log.Panic("Url is")
+	}
+
+	resp, err := http.Get(c.Url)
+	if err != nil {
+		log.Panic("Error occured. | ", err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		log.Panic("Status is not OK. | ", resp.StatusCode)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	var result interface{}
+	json.Unmarshal(body, &result)
+	return result
 }
--- net/main.go
+++ net/main.go
@@ -10,5 +10,21 @@ import (
 // - [networking - Access HTTP response as string in Go - Stack Overflow](https://stackoverflow.com/questions/38673673/access-http-response-as-string-in-go)
 func main() {
 	client := client.NewClient("https://blockchain.info//latestblock")
+	fmt.Println("GetAsString")
 	fmt.Println(client.GetAsString())
+	fmt.Println("")
+
+	fmt.Println("GetAsObject")
+	result := client.GetAsObject()
+	fmt.Printf("%#v\n", result)
+	fmt.Println("")
+
+	fmt.Println("GetAsObject parsed")
+	fmt.Printf(
+		"hash: %v\ntime: %.f\nblock_index: %.f\nheight: %.f\n",
+		result.(map[string]interface{})["hash"].(string),
+		result.(map[string]interface{})["time"].(float64),
+		result.(map[string]interface{})["block_index"].(float64),
+		result.(map[string]interface{})["height"].(float64),
+	)
 }
