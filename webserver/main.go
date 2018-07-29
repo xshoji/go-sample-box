0bfbc9672a39541bb8a09bf3b6afd411e3559292
xshoji <40511798+xshoji@users.noreply.github.com>
2018-07-29 21:00:26 +0900

    add webserver

--- webserver/main.go
+++ webserver/main.go
@@ -1,23 +1,23 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"strconv"
+
+	"github.com/jessevdk/go-flags"
 )
 
+type options struct {
+	Port int `short:"p" long:"port" description:"Listen port" default:"9090"`
+}
+
 func main() {
-	help := flag.Bool("h", false, "help")
-	port := flag.Int("port", 9090, "[optional] port")
-	flag.Parse()
-	// Required parameter
-	// - [Can Go's `flag` package print usage? - Stack Overflow](https://stackoverflow.com/questions/23725924/can-gos-flag-package-print-usage)
-	if *help == true {
-		flag.Usage()
-		os.Exit(0)
+	var opts options
+	if _, err := flags.Parse(&opts); err != nil {
+		// some error handling
+		return
 	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// Get all query strings as map
@@ -28,7 +28,7 @@ func main() {
 		fmt.Fprintf(w, "name: %s\n", query)
 		fmt.Fprintf(w, "Query strings: %v\n", params)
 	})
-	err := http.ListenAndServe(":"+strconv.Itoa(*port), nil) //監視するポートを設定します。
+	err := http.ListenAndServe(":"+strconv.Itoa(opts.Port), nil) //監視するポートを設定します。
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
