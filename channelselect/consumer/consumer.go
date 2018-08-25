7ccda66467f63accc0a8e2fa67844fbf222b0b84
xshoji <40511798+xshoji@users.noreply.github.com>
2018-08-26 05:02:40 +0900

    update

--- channelselect/consumer/consumer.go
+++ channelselect/consumer/consumer.go
@@ -11,14 +11,18 @@ import (
 
 // Consumer Consumer
 type Consumer struct {
-	Channel   <-chan string
-	Identifir string
+	Channel1    <-chan string
+	Channel2    <-chan string
+	ChannelDone <-chan bool
+	Identifir   string
 }
 
 // NewConsumer NewConsumer
-func NewConsumer(ch <-chan string) *Consumer {
+func NewConsumer(ch <-chan string, ch2 <-chan string, chDone <-chan bool) *Consumer {
 	c := new(Consumer)
-	c.Channel = ch
+	c.Channel1 = ch
+	c.Channel2 = ch2
+	c.ChannelDone = chDone
 	c.Identifir = getMD5Hash(strconv.FormatFloat(rand.Float64(), 'f', 4, 32))
 	return c
 }
@@ -34,15 +38,44 @@ func getMD5Hash(text string) string {
 func (c Consumer) Consume() {
 
 	fmt.Printf("[%v] Consumer started.\n", c.Identifir)
-	if c.Channel == nil {
-		panic("Channel is nil.")
+	if c.Channel1 == nil {
+		panic("Channel1 is nil.")
+	}
+	if c.Channel2 == nil {
+		panic("Channel2 is nil.")
+	}
+	if c.ChannelDone == nil {
+		panic("ChannelDone is nil.")
 	}
 
-	for v := range c.Channel {
+	// for v := range c.Channel {
+	// 	duration := time.Duration(rand.Intn(1000-1)+1) * time.Millisecond
+	// 	fmt.Printf("[%v] Consumer sleep: %v\n", c.Identifir, duration)
+	// 	time.Sleep(duration)
+	// 	fmt.Printf("[%v] Consumer recieved: %v\n", c.Identifir, v)
+	// }
+	printMessage := func(message string, isSuccessRecieved bool, number string) {
+		if isSuccessRecieved == false {
+			fmt.Printf("[%v] Consumer [Channel%v] exit.\n", c.Identifir, number)
+		}
 		duration := time.Duration(rand.Intn(1000-1)+1) * time.Millisecond
-		fmt.Printf("[%v] Consumer sleep: %v\n", c.Identifir, duration)
+		//		fmt.Printf("[%v] Consumer sleep: %v\n", c.Identifir, duration)
 		time.Sleep(duration)
-		fmt.Printf("[%v] Consumer recieved: %v\n", c.Identifir, v)
+		fmt.Printf("[%v] Consumer [Channel%v] [sleep:%v] recieved: %v\n", c.Identifir, number, duration, message)
 	}
-	fmt.Printf("[%v] Consumer channle closed.\n", c.Identifir)
+	func() {
+		for {
+			select {
+			case v, ok := <-c.Channel1:
+				printMessage(v, ok, "1")
+			case v, ok := <-c.Channel2:
+				printMessage(v, ok, "2")
+			case v := <-c.ChannelDone:
+				if v == true {
+					fmt.Printf("[%v] Consumer exit.\n", c.Identifir)
+					return
+				}
+			}
+		}
+	}()
 }
--- channelselect/main.go
+++ channelselect/main.go
@@ -18,6 +18,13 @@ type options struct {
 
 // - [Go の channel 処理パターン集 · Hori Blog](https://hori-ryota.com/blog/golang-channel-pattern/)
 // - [GoのChannelを使いこなせるようになるための手引 - Qiita](https://qiita.com/awakia/items/f8afa070c96d1c9a04c9)
+//-----
+// $ for ((i=0; i < 100; i++)); do openssl rand -base64 12 | fold -w 10 | head -1; done |xargs -I{} curl http://localhost:9090/channel1?name={}
+// $ for ((i=0; i < 100; i++)); do openssl rand -base64 12 | fold -w 10 | head -1; done |xargs -I{} curl http://localhost:9090/channel2?name={}
+// $ curl http://localhost:9090/done
+// $ curl http://localhost:9090/done
+// $ curl http://localhost:9090/done
+// $ curl http://localhost:9090/close
 func main() {
 
 	var opts options
@@ -27,27 +34,44 @@ func main() {
 	}
 
 	// channel capacity = 100
-	channel := make(chan string, 100)
-	producer := producer.NewProducer(channel)
+	channel1 := make(chan string, 100)
+	channel2 := make(chan string, 100)
+	channelDone := make(chan bool, 100)
+	producer1 := producer.NewProducer(channel1)
+	producer2 := producer.NewProducer(channel2)
 
 	fmt.Printf("Consuming workers : %v\n", opts.WorkerCount)
 	for i := 0; i < opts.WorkerCount; i++ {
-		consumer := consumer.NewConsumer(channel)
+		consumer := consumer.NewConsumer(channel1, channel2, channelDone)
 		go consumer.Consume()
 	}
 
 	// Add request
 	// curl http://localhost:9090?name=xshoji
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/channel1", func(w http.ResponseWriter, r *http.Request) {
 		// Get query string as single value
 		name := r.URL.Query().Get("name")
-		producer.Produce(name)
+		producer1.Produce(name)
+	})
+	// Add request
+	// curl http://localhost:9090?name=xshoji
+	http.HandleFunc("/channel2", func(w http.ResponseWriter, r *http.Request) {
+		// Get query string as single value
+		name := r.URL.Query().Get("name")
+		producer2.Produce(name)
+	})
+	// Add request
+	// curl http://localhost:9090?name=xshoji
+	http.HandleFunc("/done", func(w http.ResponseWriter, r *http.Request) {
+		channelDone <- true
 	})
 
 	// Close request
 	// curl http://localhost:9090/close
 	http.HandleFunc("/close", func(w http.ResponseWriter, r *http.Request) {
-		close(channel)
+		close(channel1)
+		close(channel2)
+		close(channelDone)
 		time.Sleep(time.Duration(100) * time.Millisecond)
 		fmt.Println("Exit server.")
 		os.Exit(0)
