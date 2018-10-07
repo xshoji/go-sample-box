90873865d508e63ed4f89f15b775920b21032370
xshoji <40511798+xshoji@users.noreply.github.com>
2018-10-07 22:22:37 +0900

    update

--- jsonDynamic/main.go
+++ jsonDynamic/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"github.com/xshoji/go-sample-box/jsonDynamic/caseSameKeyDifferentStructure"
+	"github.com/xshoji/go-sample-box/jsonDynamic/caseDifferentKeyDifferentStructure"
 )
 
 // > 動的な要素を持つJSONをいい感じにUnmarshalする - Qiita
 // > https://qiita.com/kanga/items/6929c6900ccbfa9dc933
+//
+// > Structをomitemptyする方法 - Qiita
+// > https://qiita.com/taroshin/items/59e62d8ec852c5f84bc1
 //
 // > Dynamic JSON umarshalling in Go – Nathan Smith – Medium
 // > https://medium.com/@nate510/dynamic-json-umarshalling-in-go-88095561d6a0
@@ -25,6 +29,9 @@ import (
 // > interface要素を持つstructへのJSON Unmarshal - すぎゃーんメモ
 // > https://memo.sugyan.com/entry/2018/06/23/232559
 func main() {
-	// 同じキー名だけど中身の構造が違うjsonのunmarshalの例
+	// ケース1: 同じキー名だけど中身の構造が違うjsonのunmarshalの例
 	caseSameKeyDifferentStructure.Run()
+
+	// ケース2: 違うキー名で中身の構造が違うjsonのunmarshalの例
+	caseDifferentKeyDifferentStructure.Run()
 }
