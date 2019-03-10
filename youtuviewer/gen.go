e9741adedf927549ca55053f31313b4e668022e3
xshoji <xshoji@users.noreply.github.com>
2019-03-10 16:29:32 +0900

    remove youtuviewer

--- youtuviewer/gen.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/zserge/lorca"
-
-func main() {
-	// You can also run "npm build" or webpack here, or compress assets, or
-	// generate manifests, or do other preparations for your assets.
-	lorca.Embed("assets.go", "resources")
-}
