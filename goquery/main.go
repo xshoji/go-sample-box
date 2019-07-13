c6e02b446baf042c89b24b4770666ce94b28c087
xshoji <xshoji@users.noreply.github.com>
2019-07-14 01:54:12 +0900

    update

--- goquery/main.go
+++ goquery/main.go
@@ -28,10 +28,55 @@ func main() {
 	}
 	fmt.Println("URL : " + *urlFlag)
 
+	// create document object.
 	res, _ := http.Get(*urlFlag)
 	node, _ := html.Parse(res.Body)
 	doc := goquery.NewDocumentFromNode(node)
-	for _, node := range doc.Nodes {
+	renderNodeList(doc.Nodes)
+
+	// print title tag node and inner text.
+	fmt.Println("<< basic >>")
+	titleSelection := doc.Find("title").First()
+	fmt.Println("Print html.Node")
+	renderNode(titleSelection.Nodes[0])
+	fmt.Println()
+	// get by goquery.Selection
+	fmt.Println("[goquery.Selection]      : " + titleSelection.Text())
+	// get by html.Node
+	fmt.Println("[html.Node (tag)]        : " + titleSelection.Nodes[0].Data)
+	fmt.Println("[html.Node (inner text)] : " + titleSelection.Nodes[0].FirstChild.Data)
+	fmt.Println()
+	fmt.Println()
+
+	// find image tag nodes and iterate printing an attribute value each.
+	fmt.Println("<< iteration >>")
+	fmt.Println("Print image tag src list")
+	doc.Find("img").Each(func(_ int, s *goquery.Selection) {
+		// get by goquery
+		imageUrl, _ := s.Attr("src")
+		fmt.Println("[goquery.Selection.Attr] : " + imageUrl)
+
+		// get by html.node
+		for _, attr := range s.Nodes[0].Attr {
+			fmt.Println("  [html.Node.Attr] " + attr.Key + " : " + attr.Val)
+		}
+	})
+	fmt.Println()
+	fmt.Println()
+
+	// find image tag nodes and iterate printing an attribute value each.
+	fmt.Println("<< find tags >>")
+	fmt.Println("Print input list")
+	doc.Find(`tr > td > form > input`).Each(func(_ int, s *goquery.Selection) {
+		renderNodeList(s.Nodes)
+	})
+	fmt.Println()
+	fmt.Println()
+
+}
+
+func renderNodeList(nodes []*html.Node) {
+	for _, node := range nodes {
 		renderNode(node)
 	}
 }
