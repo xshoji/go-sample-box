aba931f756db6bc7ed4dfdc918306b4a2c94fca1
xshoji <xshoji@users.noreply.github.com>
2019-09-29 18:09:54 +0900

    add

--- githubapi/main.go
+++ githubapi/main.go
@@ -1,18 +1,55 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"flag"
 	"fmt"
 	"github.com/xshoji/go-sample-box/githubapi/client"
+	"log"
 	"os"
 )
 
+var helpFlag = flag.Bool("help", false, "help")
+var commandFlag = flag.String("command", "", "[required] command")
+var user string
+var repository string
+var accessToken string
+var apiClient *client.Client
+
+func init() {
+	flag.BoolVar(helpFlag, "h", false, "= -help")
+	flag.StringVar(commandFlag, "c", "", "= -command")
+	user = os.Getenv("GITHUB_USER")
+	repository = os.Getenv("GITHUB_REPOSITORY")
+	accessToken = os.Getenv("GITHUB_ACCESS_TOKEN")
+	apiClient = client.NewClient("https://api.github.com")
+}
+
 func main() {
 
-	user := os.Getenv("GITHUB_USER")
-	repository := os.Getenv("GITHUB_REPOSITORY")
-	accessToken := os.Getenv("GITHUB_ACCESS_TOKEN")
+	flag.Parse()
+	// Required parameter
+	// - [Can Go's `flag` package print usage? - Stack Overflow](https://stackoverflow.com/questions/23725924/can-gos-flag-package-print-usage)
+	if *helpFlag || *commandFlag == "" {
+		flag.Usage()
+		os.Exit(0)
+	}
+	fmt.Println("command: ", *commandFlag)
 
-	apiClient := client.NewClient("https://api.github.com")
+	if *commandFlag == "issues" {
+		issues()
+	} else {
+		log.Fatal("Unknown command.")
+	}
+}
+
+func issues() {
 	res := apiClient.Get("/repos/" + user + "/" + repository + "/issues?access_token=" + accessToken)
-	fmt.Println(res.GetBody())
+	var buf bytes.Buffer
+	err := json.Indent(&buf, []byte(res.GetBody()), "", "  ")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(buf.String())
 }
