48196127e8aa224d69cfc1f388767387fede7645
xshoji <xshoji@users.noreply.github.com>
2019-12-02 01:15:18 +0900

    update

--- yaml-order-preserving/main.go
+++ yaml-order-preserving/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/golang/glog"
 	"gopkg.in/yaml.v2"
+	"reflect"
 	"strconv"
 	"strings"
 )
@@ -24,120 +25,210 @@ func smartMethod() {
 
 	node := &yaml.MapSlice{}
 	yaml.Unmarshal([]byte(yamlString), node)
-	orderedMapSlice := NewOrderedMapSlice(*node)
-	dumpNode("result - orderedMapSlice", orderedMapSlice.Current())
+	orderedMapSlice := NewOrderedMapSlice(node)
+	dumpNode("result - orderedMapSlice", orderedMapSlice.GetContents())
 	fmt.Printf("\n\n\n")
 	developmentTeams := orderedMapSlice.Get(`development-teams`)
-	dumpNode("result - developmentTeams", developmentTeams.Current())
+	dumpNode("result - developmentTeams", developmentTeams.GetContents())
 	fmt.Printf("\n\n\n")
 	teamA := developmentTeams.Get(`team-a`)
-	dumpNode("result - teamA", teamA.Current())
+	dumpNode("result - teamA", teamA.GetContents())
 	fmt.Printf("\n\n\n")
 	id := teamA.Get(`pc-app-name1`).Get(`id`)
-	dumpNode("result - id", id.Current())
+	dumpNode("result - id", id.GetContents())
 	fmt.Printf("\n\n\n")
 	ranks := teamA.Get(`ranks`)
-	dumpNode("result - ranks", ranks.Current())
+	dumpNode("result - ranks", ranks.GetContents())
 	fmt.Printf("\n\n\n")
 	ranksFirst := ranks.Get("0")
-	dumpNode("result - ranksFirst", ranksFirst.Current())
+	dumpNode("result - ranksFirst", ranksFirst.GetContents())
 	fmt.Printf("\n\n\n")
 	ranksSecond := ranks.Get("1")
-	dumpNode("result - ranksSecond", ranksSecond.Current())
+	dumpNode("result - ranksSecond", ranksSecond.GetContents())
 	fmt.Printf("\n\n\n")
 	ranksUndefined := ranks.Get("2")
-	dumpNode("result - ranksUndefined", ranksUndefined.Current())
+	dumpNode("result - ranksUndefined", ranksUndefined.GetContents())
+	fmt.Printf("\n\n\n")
+	undefinedValue := orderedMapSlice.Get(`hogehoge`)
+	dumpNode("undefinedValue", undefinedValue.GetContents())
 	fmt.Printf("\n\n\n")
 
-	undefinedValue := orderedMapSlice.Get(`hogehoge`)
-	dumpNode("undefinedValue", undefinedValue.Current())
+	//orderedMapSlice.Set(`development-teams`, nil)
+	//fmt.Printf("-- orderedMapSlice set development-teams %v\n", orderedMapSlice.GetContents())
+	//dumpNode("result - developmentTeams#set", orderedMapSlice.GetContents())
+
+	//orderedMapSlice.Get(`development-teams`).Set(`team-a`, nil)
+	//fmt.Printf("-- orderedMapSlice set development-teams.team-a %v\n", orderedMapSlice.GetContents())
+	//dumpNode("result - developmentTeams#set", orderedMapSlice.GetContents())
 
 }
 
 type OrderedMapSlice struct {
-	parent  *OrderedMapSlice
-	current interface{}
-	isRoot  bool
+	parent      *OrderedMapSlice
+	contents    interface{}
+	currentItem *yaml.MapItem
+	isRoot      bool
 }
 
 func NewOrderedMapSlice(yamlData interface{}) OrderedMapSlice {
-	rootMapSlice := yaml.MapSlice{yaml.MapItem{Key: 0, Value: yamlData}}
+	rootMapItem := yaml.MapItem{Key: `root`, Value: yamlData}
+	rootMapSlice := yaml.MapSlice{rootMapItem}
 	orderedMapSlice := OrderedMapSlice{
 		parent: &OrderedMapSlice{
-			parent:  nil,
-			current: rootMapSlice,
-			isRoot:  true,
+			parent:      nil,
+			contents:    rootMapSlice,
+			currentItem: nil,
+			isRoot:      true,
 		},
-		current: yamlData,
-		isRoot:  false,
+		contents:    yamlData,
+		currentItem: &rootMapItem,
+		isRoot:      false,
 	}
 	return orderedMapSlice
 }
 
-func createOrderedMapSlice(parent *OrderedMapSlice, yamlData interface{}) *OrderedMapSlice {
-	orderedMapSlice := OrderedMapSlice{
-		parent:  parent,
-		current: yamlData,
-		isRoot:  false,
+func createOrderedMapSlice(parent *OrderedMapSlice, item *yaml.MapItem, yamlData interface{}) *OrderedMapSlice {
+	return &OrderedMapSlice{
+		parent:      parent,
+		contents:    yamlData,
+		currentItem: item,
+		isRoot:      false,
 	}
-	return &orderedMapSlice
 }
 
-func (o *OrderedMapSlice) Current() interface{} {
-	if o.current == nil {
+func (o *OrderedMapSlice) GetContents() interface{} {
+	if o.contents == nil {
 		return nil
 	}
-	v, ok := o.current.(yaml.MapSlice)
+	v, ok := o.contents.(yaml.MapSlice)
 	if ok {
 		return v
 	}
-	return o.current.(interface{})
+	return o.contents.(interface{})
+}
+
+func (o *OrderedMapSlice) Parent() *OrderedMapSlice {
+	if o.parent == nil {
+		panic("Parent is nul.")
+	}
+	return o.parent
 }
 
 func (o *OrderedMapSlice) Get(key string) *OrderedMapSlice {
-	dumpNode("o.Current", o.Current())
-	fmt.Printf(">> o.current : %T, %v\n", o.current, o.current)
-	if o.current == nil {
-		return createOrderedMapSlice(o, nil)
+	dumpNode("o.GetContents", o.GetContents())
+	fmt.Printf(">> o.contents : %T, %v\n", o.contents, o.contents)
+	if o.contents == nil {
+		return createOrderedMapSlice(o, nil, nil)
 	}
-	mapSlice, ok := o.current.(yaml.MapSlice)
-	fmt.Printf("-- o.current.(yaml.MapSlice)\n")
-	fmt.Printf("---- mapSlice	: %T, %v\n", mapSlice, mapSlice)
+	mapSlice, ok := o.contents.(*yaml.MapSlice)
+	fmt.Printf("-- o.contents.(yaml.MapSlice)\n")
+	fmt.Printf("---- mapSlice	: %T, %p, %v\n", mapSlice, mapSlice, mapSlice)
 	fmt.Printf("---- ok : %v\n", ok)
 	if ok {
-		for _, item := range mapSlice {
-			fmt.Printf("---- item: %T\n", item.Value)
-			if item.Key == key {
-				v, ok := item.Value.(yaml.MapSlice)
+		for index, item := range *mapSlice {
+			referencedItem := &(*mapSlice)[index]
+			fmt.Printf("---- item.Value: %T, item: %p, key: %v, value: %v, value-pointer: %p, value-pointers pointer: %v\n", referencedItem.Value, referencedItem, referencedItem.Key, referencedItem.Value, referencedItem.Value, &referencedItem.Value)
+			if referencedItem.Key == key {
+				v, ok := referencedItem.Value.(yaml.MapSlice)
 				if ok {
-					return createOrderedMapSlice(o, v)
+					return createOrderedMapSlice(o, referencedItem, &v)
 				}
-				v2 := item.Value.(interface{})
-				return createOrderedMapSlice(o, v2)
+				return createOrderedMapSlice(o, nil, &item.Value)
 			}
 		}
 	}
-	slice, ok := o.current.([]interface{})
-	fmt.Printf("--o.current.([]interface{})\n")
+	slice, ok := o.contents.(*interface{})
+	fmt.Printf("--o.contents.(*interface{})\n")
 	fmt.Printf("---- slice : %T, %v\n", slice, slice)
 	fmt.Printf("---- ok : %v\n", ok)
 	if ok {
-		for i, item := range slice {
-			index := strconv.FormatInt(int64(i), 10)
-			fmt.Printf("---- index : %v, key : %v\n", index, key)
-			fmt.Printf("---- index == key : %v\n", index == key)
-			if index == key {
-				fmt.Printf("---- item: %T\n", item)
-				v, ok := item.(yaml.MapSlice)
-				if ok {
-					return createOrderedMapSlice(o, &v)
+		// > go - range over interface{} which stores a slice - Stack Overflow
+		// > https://stackoverflow.com/questions/14025833/range-over-interface-which-stores-a-slice?answertab=active#tab-top
+		switch reflect.TypeOf(*slice).Kind() {
+		case reflect.Slice:
+			fmt.Printf(">> slice is slice!\n")
+			s := reflect.ValueOf(*slice)
+			for i := 0; i < s.Len(); i++ {
+				index := strconv.FormatInt(int64(i), 10)
+				fmt.Printf("---- index : %v, key : %v\n", index, key)
+				fmt.Printf("---- index == key : %v\n", index == key)
+				if index == key {
+					fmt.Printf("---- item: %T\n", s.Index(i).Interface())
+					return createOrderedMapSlice(o, nil, s.Index(i).Interface())
 				}
-				v2 := item.(interface{})
-				return createOrderedMapSlice(o, &v2)
 			}
 		}
+
 	}
-	return createOrderedMapSlice(o, nil)
+	return createOrderedMapSlice(o, nil, nil)
+}
+
+func (o *OrderedMapSlice) Set(key string, value interface{}) {
+	//success := false
+	orderedMapSlice := o.Get(key)
+	if orderedMapSlice == nil {
+		return
+	}
+	fmt.Printf(">>>> item.Value: %T, item: %p, key: %v, value: %v, value-pointer: %p, value-pointers pointer: %v\n", orderedMapSlice.currentItem.Value, orderedMapSlice.currentItem, orderedMapSlice.currentItem.Key, orderedMapSlice.currentItem.Value, orderedMapSlice.currentItem.Value, &orderedMapSlice.currentItem.Value)
+	printPointer("@@@@@ pointer orderedMapSlice.currentItem ", orderedMapSlice.currentItem, orderedMapSlice.currentItem)
+	orderedMapSlice.currentItem.Value = value
+	fmt.Printf(">>>> item.Value: %T, item: %p, key: %v, value: %v, value-pointer: %p, value-pointers pointer: %v\n", orderedMapSlice.currentItem.Value, orderedMapSlice.currentItem, orderedMapSlice.currentItem.Key, orderedMapSlice.currentItem.Value, orderedMapSlice.currentItem.Value, &orderedMapSlice.currentItem.Value)
+	printPointer("@@@@@ pointer orderedMapSlice.currentItem ", orderedMapSlice.currentItem, orderedMapSlice.currentItem)
+
+	//printPointer("@@@@@ pointer o          ", o, o)
+	//printPointer("@@@@@ pointer reGetParent", reGetParent, reGetParent)
+
+	//newCurrent := o.Get(key).contents
+	//newOrdered := o.Get(key)
+	//reGetCurrent := orderedMapSlice.Parent().Get(key).GetContents()
+	//reGetOrderedMapSliceFromO := o.Get(key)
+	//reGetOrdered := orderedMapSlice.Parent().Get(key)
+	//
+	//
+	//
+	//fmt.Printf(">>>> reGetOrderedMapSliceFromO item.Value: %T, item: %p, key: %v, value: %v, value-pointer: %p, value-pointers pointer: %v\n", reGetOrderedMapSliceFromO.currentItem.Value, reGetOrderedMapSliceFromO.currentItem, reGetOrderedMapSliceFromO.currentItem.Key, reGetOrderedMapSliceFromO.currentItem.Value, reGetOrderedMapSliceFromO.currentItem.Value, &reGetOrderedMapSliceFromO.currentItem.Value)
+	//printPointer("@@@@@ pointer reGetOrderedMapSliceFromO.currentItem ", reGetOrderedMapSliceFromO.currentItem, reGetOrderedMapSliceFromO.currentItem)
+	//fmt.Printf(">>>> reGetOrdered item.Value             : %T, item: %p, key: %v, value: %v, value-pointer: %p, value-pointers pointer: %v\n", reGetOrdered.currentItem.Value, reGetOrdered.currentItem, reGetOrdered.currentItem.Key, reGetOrdered.currentItem.Value, reGetOrdered.currentItem.Value, &reGetOrdered.currentItem.Value)
+	//printPointer("@@@@@ pointer reGetOrdered.currentItem              ", reGetOrdered.currentItem, reGetOrdered.currentItem)
+
+	//reGetParent := o.Get(key).Parent()
+	//reGetOrderedMapSlice := reGetParent.Get(key)
+	//printPointer("@@@@@ pointer orderedMapSlice             ", orderedMapSlice, orderedMapSlice)
+	//printPointer("@@@@@ pointer orderedMapSlice.contents    ", orderedMapSlice.contents, orderedMapSlice.contents)
+	//printPointer("@@@@@ pointer orderedMapSlice.currentItem ", *orderedMapSlice.currentItem, orderedMapSlice.currentItem)
+	//printPointer("@@@@@ pointer reGetOrderedMapSlice.contents     ", reGetOrderedMapSlice.contents, reGetOrderedMapSlice.contents)
+	//printPointer("@@@@@ pointer reGetOrderedMapSliceFromO.contents", reGetOrderedMapSliceFromO.contents, reGetOrderedMapSliceFromO.contents)
+	//
+	//printPointer("@@@@@ pointer orderedMapSlice                  ", orderedMapSlice, orderedMapSlice)
+	//printPointer("@@@@@ pointer orderedMapSlice.contents          ", orderedMapSlice.contents, orderedMapSlice.contents)
+	//printPointer("@@@@@ pointer reGetOrderedMapSlice.contents     ", reGetOrderedMapSlice.contents, reGetOrderedMapSlice.contents)
+	//printPointer("@@@@@ pointer reGetOrderedMapSliceFromO.contents", reGetOrderedMapSliceFromO.contents, reGetOrderedMapSliceFromO.contents)
+	//
+	//fmt.Printf("@@@@@ set orderedMapSlice.GetContents() : %v\n", orderedMapSlice.GetContents())
+	//fmt.Printf("@@@@@ set orderedMapSlice.contents : %v\n", orderedMapSlice.contents)
+	////fmt.Printf("@@@@@ set o.Get(key).contents : %v\n", newCurrent)
+	//printPointer("@@@@@ pointer orderedMapSlice.GetContents()", orderedMapSlice.GetContents(), orderedMapSlice.GetContents())
+	//printPointer("@@@@@ pointer orderedMapSlice.contents", orderedMapSlice.contents, orderedMapSlice.contents)
+	//printPointer("@@@@@ pointer reGetCurrent", reGetCurrent, reGetCurrent)
+	//printPointer("@@@@@ pointer reGetOrdered", reGetOrdered, reGetOrdered)
+	//printPointer("@@@@@ pointer orderedMapSlice", orderedMapSlice, orderedMapSlice)
+	//printPointer("@@@@@ pointer o.Get(key).contents", newCurrent, newCurrent)
+	//printPointer("@@@@@ pointer orderedMapSlice.Parent()", orderedMapSlice.Parent(), orderedMapSlice.Parent())
+	//printPointer("@@@@@ pointer newOrdered.Parent()", newOrdered.Parent(), newOrdered.Parent())
+
+	//mapSlice, ok := orderedMapSlice.contents.(*yaml.MapSlice)
+	//fmt.Printf("-- orderedMapSlice.contents.(yaml.MapSlice)\n")
+	//fmt.Printf("---- mapSlice	: %T, %v\n", mapSlice, mapSlice)
+	//fmt.Printf("---- ok : %v\n", ok)
+	//if ok {
+	//	for i, item := range *mapSlice {
+	//		fmt.Printf("---- item: %T, item.Key: %v, key: %v\n", item.Value, item.Key, key)
+	//		if item.Key == key {
+	//			(*mapSlice)[i].Value = value
+	//		}
+	//	}
+	//}
+	//fmt.Printf("---- mapSlice after : %T, %v\n", mapSlice, mapSlice)
 }
 
 func dumpNode(name string, node interface{}) {
