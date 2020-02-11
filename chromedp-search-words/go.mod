a65ccd4757c1533cf423514fdd3cfe7b43a51e48
xshoji <xshoji@users.noreply.github.com>
2020-02-12 00:49:38 +0900

    update chromedp samples

--- chromedp-search-words/go.mod
+++ chromedp-search-words/go.mod
@@ -3,15 +3,8 @@ module github.com/xshoji/go-sample-box/chromedp-search-words
 go 1.13
 
 require (
-	github.com/chromedp/cdproto v0.0.0-20190316014124-183f1739f584
-	github.com/chromedp/chromedp v0.1.3
-	github.com/disintegration/imaging v1.6.0
-	github.com/gorilla/websocket v1.4.0
+	github.com/chromedp/chromedp v0.5.3
 	github.com/jessevdk/go-flags v1.4.0
-	github.com/knq/snaker v0.0.0-20181215144011-2bc8a4db4687
-	github.com/knq/sysutil v0.0.0-20181215143952-f05b59f0f307
-	github.com/mailru/easyjson v0.0.0-20190312143242-1de009706dbe
-	github.com/yosssi/gohtml v0.0.0-20190128141317-9b7db94d32d9
-	golang.org/x/image v0.0.0-20190227222117-0694c2d4d067
-	golang.org/x/net v0.0.0-20190313220215-9f648a60d977
+	github.com/yosssi/gohtml v0.0.0-20190915184251-7ff6f235ecaf
+	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
 )
--- chromedp-search-words/main.go
+++ chromedp-search-words/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"github.com/chromedp/chromedp"
-	"github.com/chromedp/chromedp/runner"
 	"github.com/jessevdk/go-flags"
 	"github.com/yosssi/gohtml"
 	"log"
@@ -16,6 +15,7 @@ import (
 type options struct {
 	Words   string `short:"w" long:"words" description:"Search words" required:"true"`
 	Service string `short:"s" long:"service" description:"Service type [ yahoo | tabelog ]" default:"yahoo"`
+	Debug   bool   `short:"d" long:"debug" description:"Debug mode"`
 }
 
 // [ Usage ]
@@ -42,36 +42,32 @@ func main() {
 	var err error
 
 	// create context
-	ctxt, cancel := context.WithCancel(context.Background())
-
-	// create chrome instance
-	c, err := chromedp.New(ctxt, chromedp.WithLog(log.Printf), chromedp.WithRunnerOptions(
-		runner.Flag("headless", true),
-		runner.Flag("disable-gpu", true),
-		runner.Flag("no-first-run", true),
-		runner.Flag("no-default-browser-check", true),
-		runner.RemoteDebuggingPort(9222),
-	))
-	//	c, err := chromedp.New(ctxt, chromedp.WithLog(log.Printf))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// defer handling
+	// > go - How to use Chrome headless with chromedp? - Stack Overflow
+	// > https://stackoverflow.com/questions/44067030/how-to-use-chrome-headless-with-chromedp
+	// > How to run chromedp on foreground? · Issue #495 · chromedp/chromedp
+	// > https://github.com/chromedp/chromedp/issues/495
+	ctxt, cancel := chromedp.NewExecAllocator(context.Background(), append(
+		chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("headless", true),
+		chromedp.Flag("disable-gpu", true),
+		chromedp.Flag("no-first-run", true),
+		chromedp.Flag("no-default-browser-check", true),
+	)...,
+	)
 	defer cancel()
-	shutdownFunc := func() {
-		err = c.Shutdown(ctxt)
-		if err != nil {
-			log.Fatal(err)
-		}
+	loggingContextOption := chromedp.WithLogf(log.Printf)
+	if opts.Debug {
+		// debug log mode
+		loggingContextOption = chromedp.WithDebugf(log.Printf)
 	}
-	defer shutdownFunc()
+	ctxt, cancel = chromedp.NewContext(ctxt, loggingContextOption)
+	defer cancel()
+	// handle kill signal
 	signals := make(chan os.Signal, 1)
 	signal.Notify(signals, os.Kill, os.Interrupt)
 	go func() {
 		<-signals
 		cancel()
-		shutdownFunc()
 		os.Exit(0)
 	}()
 
@@ -84,7 +80,7 @@ func main() {
 		tasks = searchTasksYahoo(opts.Words, &res)
 	}
 
-	err = c.Run(ctxt, tasks)
+	err = chromedp.Run(ctxt, tasks)
 	if err != nil {
 		log.Fatal(err)
 	}
