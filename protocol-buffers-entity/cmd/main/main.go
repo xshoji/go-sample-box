c53c6cbbb74e38d053521145f655afb71101feb2
xshoji <xshoji@users.noreply.github.com>
2020-04-24 02:58:23 +0900

    update

--- protocol-buffers-entity/cmd/main/main.go
+++ protocol-buffers-entity/cmd/main/main.go
@@ -16,6 +16,10 @@ func main() {
 		return t
 	}
 
+	// どうやらnullはjson化した際設定されない思想らしい。
+	// > why protobuf optional field does not take null - Google グループ
+	// > https://groups.google.com/forum/#!topic/protobuf/KcLoxlJGVMY
+	// Protocol buffers has no concept of null.  Fields cannot be set to null.  You can *clear* a field, like:
 	user := &entity.User{
 		Id:               1,
 		Name:             `a`,
@@ -24,19 +28,19 @@ func main() {
 		Birth: &entity.Birth{
 			Datetime:         getTimestamp(),
 			WeightOptional:   &entity.Birth_Weight{Weight: 12},
-			HospitalOptional: &entity.Birth_Hospital{Hospital: "hugeHospital"},
+			HospitalOptional: nil,
 		},
 		Addresss: &entity.Address{
 			Country:         entity.Country_JP,
 			ZipCodeOptional: &entity.Address_ZipCode{ZipCode: 1},
 			StateOptional:   &entity.Address_State{State: "test"},
-			CityOptional:    &entity.Address_City{City: "city"},
+			CityOptional:    nil,
 		},
 	}
 
 	m := jsonpb.Marshaler{
 		EmitDefaults: true,
-		Indent:       "    ",
+		Indent:       "  ",
 		OrigName:     true,
 	}
 	m.Marshal(os.Stdout, user)
