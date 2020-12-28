660b42e60132c9cad853f177956d43226cd4ef2d
xshoji <xshoji@users.noreply.github.com>
2020-12-28 22:02:03 +0900

    add evaluate js sample.

---
+++ chromedp/cmd/evaluatejs/main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"github.com/chromedp/chromedp"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+)
+
+var (
+	arguments = struct {
+		url        *string
+		debug      *bool
+		noHeadless *bool
+		help       *bool
+	}{
+		flag.String("u", "" /*      */, "[Required] URL"),
+		flag.Bool("d", false /*   */, "\n[Optional] Debug mode"),
+		flag.Bool("n", false /*   */, "\n[Optional] Disable Headless mode"),
+		flag.Bool("h", false /*   */, "\nhelp"),
+	}
+)
+
+// [ Usage ]
+// go run cmd/gethtml/main.go -u="https://news.yahoo.co.jp/" -q="#liveStream"
+// go run cmd/gethtml/main.go -u="https://news.yahoo.co.jp/" -q="section.toptopics"
+func main() {
+
+	flag.Parse()
+	// Required parameter
+	// - [Can Go's `flag` package print usage? - Stack Overflow](https://stackoverflow.com/questions/23725924/can-gos-flag-package-print-usage)
+	if *arguments.help || *arguments.url == "" {
+		flag.Usage()
+		os.Exit(0)
+	}
+	var err error
+
+	// create context
+	// > go - How to use Chrome headless with chromedp? - Stack Overflow
+	// > https://stackoverflow.com/questions/44067030/how-to-use-chrome-headless-with-chromedp
+	// > How to run chromedp on foreground? · Issue #495 · chromedp/chromedp
+	// > https://github.com/chromedp/chromedp/issues/495
+	ctxt, cancel := chromedp.NewExecAllocator(context.Background(), append(
+		chromedp.DefaultExecAllocatorOptions[:],
+		chromedp.Flag("headless", !*arguments.noHeadless),
+		chromedp.Flag("disable-gpu", true),
+		chromedp.Flag("no-first-run", true),
+		chromedp.Flag("no-default-browser-check", true),
+	)...,
+	)
+	defer cancel()
+	loggingContextOption := chromedp.WithLogf(log.Printf)
+	if *arguments.debug {
+		// debug log mode
+		loggingContextOption = chromedp.WithDebugf(log.Printf)
+	}
+	ctxt, cancel = chromedp.NewContext(ctxt, loggingContextOption)
+	defer cancel()
+	// handle kill signal
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Kill, os.Interrupt)
+	go func() {
+		<-signals
+		cancel()
+		os.Exit(0)
+	}()
+
+	// run task list
+	var res1 string
+	var res2 string
+	err = chromedp.Run(ctxt, createTasks(*arguments.url, &res1, &res2))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Printf("\nres1: \n%s\n\n", res1)
+	log.Printf("\nres2: \n%s\n\n", res2)
+}
+
+func createTasks(url string, res1 *string, res2 *string) chromedp.Tasks {
+	js1 := `
+(() => {
+  const bodyElement = document.querySelector("body");
+  const textContent = bodyElement.children[0].textContent;
+  return textContent;
+})()
+`
+	return chromedp.Tasks{
+		chromedp.Navigate(url),
+		chromedp.Sleep(2 * time.Second),
+		chromedp.Evaluate(js1, res1, chromedp.EvalIgnoreExceptions),
+		chromedp.Evaluate(`document.title;`, res2, chromedp.EvalIgnoreExceptions),
+	}
+}
