c0875144f036b364050e2c866eb8bf20e3989e0e
xshoji <xshoji@users.noreply.github.com>
2023-02-17 10:06:02 +0900

    fix

--- reflection/main.go
+++ reflection/main.go
@@ -15,23 +15,36 @@ func (p *Person) GetGender() int8 {
 	return p.gender
 }
 
+func (p *Person) getGender() int8 {
+	return p.gender
+}
+
 func main() {
+
+	s := "s"
+	ts := reflect.TypeOf(s)
+	fmt.Println("ts: ", ts)
+	fmt.Println("ts.Name(): ", ts.Name())
+	fmt.Println("ts.Kind(): ", ts.Kind())
+
 	p := Person{
 		Name:   "aaa",
 		Age:    16,
 		gender: 1,
 	}
-	t := reflect.TypeOf(p)
-	fmt.Println("t: ", t)
-	fmt.Println("t.Name(): ", t.Name())
-	fmt.Println("t.NumField(): ", t.NumField())
-	for i := 0; i < t.NumField(); i++ {
-		fmt.Println(t.Field(i), t.Field(i).IsExported())
+	tp := reflect.TypeOf(p)
+	fmt.Println("tp: ", tp)
+	fmt.Println("tp.Name(): ", tp.Name())
+	if reflect.Struct == tp.Kind() {
+		fmt.Println("tp.Kind(): ", tp.Kind())
 	}
-	pt := reflect.PtrTo(t)
+	fmt.Println("tp.NumField(): ", tp.NumField())
+	for i := 0; i < tp.NumField(); i++ {
+		fmt.Println(tp.Field(i), tp.Field(i).Type, tp.Field(i).IsExported())
+	}
+	pt := reflect.PtrTo(tp)
 	fmt.Println("pt.NumMethod(): ", pt.NumMethod())
 	for i := 0; i < pt.NumMethod(); i++ {
 		fmt.Println(pt.Method(i), pt.Method(i).IsExported())
 	}
-
 }
