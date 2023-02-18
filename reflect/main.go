7805dfa6dc008bacf96d3e2cad267b8ad96b4d01
xshoji <xshoji@users.noreply.github.com>
2023-02-19 01:07:38 +0900

    fix

--- reflect/main.go
+++ reflect/main.go
@@ -15,6 +15,7 @@ func (p *Person) GetGender() int8 {
 	return p.gender
 }
 
+// Cannot access private method, even via reflect.
 func (p *Person) getGender() int8 {
 	return p.gender
 }
@@ -23,9 +24,9 @@ func main() {
 
 	s := "s"
 	ts := reflect.TypeOf(s)
-	fmt.Println("ts: ", ts)
-	fmt.Println("ts.Name(): ", ts.Name())
-	fmt.Println("ts.Kind(): ", ts.Kind())
+	fmt.Println("ts:", ts)
+	fmt.Println("  ts.Name():", ts.Name())
+	fmt.Println("  ts.Kind():", ts.Kind())
 
 	p := Person{
 		Name:   "aaa",
@@ -33,18 +34,33 @@ func main() {
 		gender: 1,
 	}
 	tp := reflect.TypeOf(p)
-	fmt.Println("tp: ", tp)
-	fmt.Println("tp.Name(): ", tp.Name())
+	fmt.Println("tp:", tp)
+	fmt.Println("  tp.Name():", tp.Name())
 	if reflect.Struct == tp.Kind() {
-		fmt.Println("tp.Kind(): ", tp.Kind())
+		fmt.Println("  tp.Kind():", tp.Kind())
 	}
-	fmt.Println("tp.NumField(): ", tp.NumField())
+	fmt.Println("  tp.NumField():", tp.NumField())
 	for i := 0; i < tp.NumField(); i++ {
-		fmt.Println(tp.Field(i), tp.Field(i).Type, tp.Field(i).IsExported())
+		fmt.Println("    tp.Field(i), tp.Field(i).Type, tp.Field(i).IsExported():", tp.Field(i), tp.Field(i).Type, tp.Field(i).IsExported())
+	}
+	vp := reflect.ValueOf(p)
+	fmt.Println("vp: ", vp)
+	fmt.Println("  vp.NumField(): ", vp.NumField())
+	for i := 0; i < vp.NumField(); i++ {
+		fmt.Println("    vp.Field(i), vp.Field(i).Type():", vp.Field(i), vp.Field(i).Type())
+		fmt.Println("      vp.Field(i).IsValid():     ", vp.Field(i).IsValid())
+		fmt.Println("      vp.Field(i).IsZero():      ", vp.Field(i).IsZero())
+		fmt.Println("      vp.Field(i).CanAddr():     ", vp.Field(i).CanAddr())
+		fmt.Println("      vp.Field(i).CanSet():      ", vp.Field(i).CanSet())
+		fmt.Println("      vp.Field(i).CanComplex():  ", vp.Field(i).CanComplex())
+		fmt.Println("      vp.Field(i).CanInt():      ", vp.Field(i).CanInt())
+		fmt.Println("      vp.Field(i).CanInterface():", vp.Field(i).CanInterface())
 	}
 	pt := reflect.PtrTo(tp)
-	fmt.Println("pt.NumMethod(): ", pt.NumMethod())
+	fmt.Println("pt:", pt)
+	fmt.Println("  pt.NumMethod():", pt.NumMethod())
 	for i := 0; i < pt.NumMethod(); i++ {
-		fmt.Println(pt.Method(i), pt.Method(i).IsExported())
+		fmt.Println("    pt.Method(i), pt.Method(i).IsExported():", pt.Method(i), pt.Method(i).IsExported())
 	}
+
 }
