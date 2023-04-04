587eea0198e2da17df855596ff98247134e8c70d
xshoji <xshoji@users.noreply.github.com>
2023-04-04 12:53:24 +0900

    fix

--- flag/cmd/longparameter/longparameter.go
+++ flag/cmd/longparameter/longparameter.go
@@ -1,135 +1,135 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
-const Usage = ""
+const Usage = "########"
 
-//> go - Flag command line parsing in golang - Stack Overflow
-//> https://stackoverflow.com/questions/19761963/flag-command-line-parsing-in-golang
+// go - Flag command line parsing in golang - Stack Overflow
+// https://stackoverflow.com/questions/19761963/flag-command-line-parsing-in-golang
 
 var (
-	//
 	// Define long parameters ( don't set default value ).
 	//
 	// Required parameters
-	paramsApple    = flag.Int("apple" /*          */, 0, Usage)
-	paramsIterator = flag.String("iterator" /*    */, "", Usage)
-	paramsTitle    = flag.String("title" /*       */, "", Usage)
+	paramsAdd      = flag.Int("a", 0, Usage)
+	paramsIncrease = flag.String("i", "", Usage)
+	paramsTitle    = flag.String("t", "", Usage)
 	// Optional parameters
-	paramsFlagship   = flag.Int("flagship" /*       */, 0, Usage)
-	paramsCount      = flag.Int("count" /*          */, 0, Usage)
-	paramsBridge     = flag.String("bridge" /*      */, "", Usage)
-	paramsEstimation = flag.String("estimation" /*  */, "", Usage)
-	paramsGulp       = flag.Bool("gulp" /*          */, false, Usage)
-	paramsHelp       = flag.Bool("help" /*          */, false, Usage)
-	paramsDebug      = flag.Bool("debug" /*         */, false, Usage)
+	paramsFilesize    = flag.Int("f", 0, Usage)
+	paramsCount       = flag.Int("c", 0, Usage)
+	paramsBinary      = flag.String("b", "", Usage)
+	paramsEnvironment = flag.String("e", "", Usage)
+	paramsGlobal      = flag.Bool("g", false, Usage)
+	paramsHelp        = flag.Bool("h", false, Usage)
+	paramsDebug       = flag.Bool("d", false, Usage)
 )
 
 func init() {
-	//
 	// Define short parameters and description ( set default value here if you need ).
 	//
 	// Required parameters
-	flag.IntVar(paramsApple /*         */, "a", 0 /*     */, "[required] apple")
-	flag.StringVar(paramsIterator /*   */, "i", "" /*    */, "[required] iterator")
-	flag.StringVar(paramsTitle /*      */, "t", "" /*    */, "[required] title")
+	flag.IntVar(paramsAdd /*            */, "add" /*         */, 0 /*          */, "[required] add")
+	flag.StringVar(paramsIncrease /*    */, "increase" /*    */, "" /*         */, "[required] increase")
+	flag.StringVar(paramsTitle /*       */, "title" /*       */, "" /*         */, "[required] title")
 	// Optional parameters
-	flag.IntVar(paramsFlagship /*      */, "f", 10 /*    */, "[optional] flagship")
-	flag.IntVar(paramsCount /*         */, "c", 1 /*     */, "[optional] count")
-	flag.StringVar(paramsBridge /*     */, "b", "BIG" /* */, "[optional] bridge")
-	flag.StringVar(paramsEstimation /* */, "e", "NO" /*  */, "[optional] estimation")
-	flag.BoolVar(paramsGulp /*         */, "g", false /* */, "\ngulp")
-	flag.BoolVar(paramsHelp /*         */, "h", false /* */, "\nhelp")
-	flag.BoolVar(paramsDebug /*        */, "d", false /* */, "\ndebug")
+	flag.IntVar(paramsFilesize /*       */, "filesize" /*    */, 10 /*         */, "[optional] filesize")
+	flag.IntVar(paramsCount /*          */, "count" /*       */, 1 /*          */, "[optional] count")
+	flag.StringVar(paramsBinary /*      */, "binary" /*      */, "00010101" /* */, "[optional] binary")
+	flag.StringVar(paramsEnvironment /* */, "environment" /* */, "DEV" /*      */, "[optional] environment")
+	flag.BoolVar(paramsGlobal /*        */, "global" /*      */, false /*      */, "global")
+	flag.BoolVar(paramsHelp /*          */, "help" /*        */, false /*      */, "help")
+	flag.BoolVar(paramsDebug /*         */, "debug" /*       */, false /*      */, "debug")
 }
 
 // << Execution sample >>
 //
 // $ go run cmd/longparameter/longparameter.go -i "param1" -t "param2" -a 100
-// apple:  100
-// bridge:  BIG
-// estimation:  NO
-// flagship:  10
-// gulp:  false
-// iterator:  param1
-// title:  param2
-// count:  1
-// debug:  false
+// add: 100
+// binary: 00010101
+// environment: DEV
+// filesize: 10
+// global: false
+// increase: param1
+// title: param2
+// count: 1
+// debug: false
 //
-// $ go run cmd/longparameter/longparameter.go -iterator "param1" -title "param2" -apple 100
-// apple:  100
-// bridge:  BIG
-// estimation:  NO
-// flagship:  10
-// gulp:  false
-// iterator:  param1
-// title:  param2
-// count:  1
-// debug:  false
+// $ $ go run cmd/longparameter/longparameter.go -increase "param1" -title "param2" -add 100
+// add: 100
+// binary: 00010101
+// environment: DEV
+// filesize: 10
+// global: false
+// increase: param1
+// title: param2
+// count: 1
+// debug: false
 //
 // $ go run cmd/longparameter/longparameter.go -h
-// Usage of /var/folders/2c/_9j92fnj5z3754dw8_h345zc0000gn/T/go-build576006264/b001/exe/longparameter:
-//  -a int
-//    	[required] apple
-//  -apple int
-//
-//  -b string
-//    	[optional] bridge (default "BIG")
-//  -bridge string
-//
-//  -c int
-//    	[optional] count (default 1)
-//  -count int
-//
-//  -d
-//    	debug
-//  -debug
-//
-//  -e string
-//    	[optional] estimation (default "NO")
-//  -estimation string
-//
-//  -f int
-//    	[optional] flagship (default 10)
-//  -flagship int
-//
-//  -g
-//    	gulp
-//  -gulp
-//
-//  -h
-//    	help
-//  -help
-//
-//  -i string
-//    	[required] iterator
-//  -iterator string
-//
-//  -t string
-//    	[required] title
-//  -title string
-//
+// Usage of /var/folders/_q/dpw924t12bj25568xfxcd2wm0000gn/T/go-build1111939584/b001/exe/longparameter:
+//   -a int
+//   -add int
+//     	[required] add
+//   -b string
+//   -binary string
+//     	[optional] binary (default "00010101")
+//   -c int
+//   -count int
+//     	[optional] count (default 1)
+//   -d
+//   -debug
+//     	debug
+//   -e string
+//   -environment string
+//     	[optional] environment (default "DEV")
+//   -f int
+//   -filesize int
+//     	[optional] filesize (default 10)
+//   -g
+//   -global
+//     	global
+//   -h
+//   -help
+//     	help
+//   -i string
+//   -increase string
+//     	[required] increase
+//   -t string
+//   -title string
+//     	[required] title
+
 func main() {
 
+	// Set Usage
+	var b bytes.Buffer
+	flag.CommandLine.SetOutput(&b)
+	flag.Usage()
+	usage := strings.Replace(b.String(), "\n    \t"+Usage, "", -1)
+	usage = strings.Replace(usage, "\t"+Usage, "", -1)
+	flag.CommandLine.SetOutput(os.Stderr)
+	flag.Usage = func() {
+		_, _ = fmt.Fprintf(flag.CommandLine.Output(), usage)
+	}
+
 	flag.Parse()
-	// Required parameter
-	// - [Can Go's `flag` package print usage? - Stack Overflow](https://stackoverflow.com/questions/23725924/can-gos-flag-package-print-usage)
-	if *paramsHelp || *paramsApple == 0 || *paramsIterator == "" || *paramsTitle == "" {
+	if *paramsHelp || *paramsAdd == 0 || *paramsIncrease == "" || *paramsTitle == "" {
 		flag.Usage()
 		os.Exit(0)
 	}
 
-	fmt.Println("apple: ", *paramsApple)
-	fmt.Println("bridge: ", *paramsBridge)
-	fmt.Println("estimation: ", *paramsEstimation)
-	fmt.Println("flagship: ", *paramsFlagship)
-	fmt.Println("gulp: ", *paramsGulp)
-	fmt.Println("iterator: ", *paramsIterator)
-	fmt.Println("title: ", *paramsTitle)
-	fmt.Println("count: ", *paramsCount)
-	fmt.Println("debug: ", *paramsDebug)
+	fmt.Println("add:", *paramsAdd)
+	fmt.Println("binary:", *paramsBinary)
+	fmt.Println("environment:", *paramsEnvironment)
+	fmt.Println("filesize:", *paramsFilesize)
+	fmt.Println("global:", *paramsGlobal)
+	fmt.Println("increase:", *paramsIncrease)
+	fmt.Println("title:", *paramsTitle)
+	fmt.Println("count:", *paramsCount)
+	fmt.Println("debug:", *paramsDebug)
 }
