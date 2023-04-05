01ebe81489eccee903a1ad507170c8940debefe7
xshoji <xshoji@users.noreply.github.com>
2023-04-05 23:38:00 +0900

    fix

--- flag/cmd/simple/simple.go
+++ flag/cmd/simple/simple.go
@@ -6,91 +6,77 @@ import (
 	"os"
 )
 
-//> go - Flag command line parsing in golang - Stack Overflow
-//> https://stackoverflow.com/questions/19761963/flag-command-line-parsing-in-golang
+// go - Flag command line parsing in golang - Stack Overflow
+// https://stackoverflow.com/questions/19761963/flag-command-line-parsing-in-golang
 
 var (
-	//
 	// Define short parameters and description ( set default value here if you need ).
 	//
-	// Required parameters
-	paramsApple    = flag.Int("a", 0 /*        */, "[required] apple")
-	paramsIterator = flag.String("i", "" /*    */, "[required] iterator")
-	paramsTitle    = flag.String("t", "" /*    */, "[required] title")
-	// Optional parameters
-	paramsFlagship   = flag.Int("f", 10 /*       */, "[optional] flagship")
-	paramsCount      = flag.Int("c", 1 /*        */, "[optional] count")
-	paramsBridge     = flag.String("b", "BIG" /* */, "[optional] bridge")
-	paramsEstimation = flag.String("e", "NO" /*  */, "[optional] estimation")
-	paramsGulp       = flag.Bool("g", false /*   */, "\ngulp")
-	paramsHelp       = flag.Bool("h", false /*   */, "\nhelp")
-	paramsDebug      = flag.Bool("d", false /*   */, "\ndebug")
+	paramsAdd         = flag.Int("a" /*    */, 0 /*       */, "[required] add")
+	paramsIncrease    = flag.String("i" /* */, "" /*      */, "[required] increase")
+	paramsTitle       = flag.String("t" /* */, "" /*      */, "[required] title")
+	paramsFilesize    = flag.Int("f" /*    */, 10 /*      */, "[optional] filesize")
+	paramsCount       = flag.Int("c" /*    */, 1 /*       */, "[optional] count")
+	paramsBinary      = flag.String("b" /* */, "00101" /* */, "[optional] binary")
+	paramsEnvironment = flag.String("e" /* */, "DEV" /*   */, "[optional] environment")
+	paramsGlobal      = flag.Bool("g" /*   */, false /*   */, "\nglobal")
+	paramsHelp        = flag.Bool("h" /*   */, false /*   */, "\nhelp")
+	paramsDebug       = flag.Bool("d" /*   */, false /*   */, "\ndebug")
 )
 
 // << Execution sample >>
 //
 // $ go run cmd/simple/simple.go -i "param1" -t "param2" -a 100
-// apple:  100
-// bridge:  BIG
-// estimation:  NO
-// forget:  10
-// gulp:  false
-// iterator:  param1
-// title:  param2
-// count:  1
-// debug:  false
-//
-// $ go run cmd/simple/simple.go -iterator "param1" -title "param2" -apple 100
-// apple:  100
-// bridge:  BIG
-// estimation:  NO
-// forget:  10
-// gulp:  false
-// iterator:  param1
-// title:  param2
-// count:  1
-// debug:  false
+// add: 100
+// binary: 00101
+// environment: DEV
+// filesize: 10
+// global: false
+// increase: param1
+// title: param2
+// count: 1
+// debug: false
 //
 // $ go run cmd/simple/simple.go -h
 // Usage of /var/folders/2c/_9j92fnj5z3754dw8_h345zc0000gn/T/go-build548286484/b001/exe/simple:
-//   -a int
-//         [required] apple
-//   -b string
-//         [optional] bridge (default "BIG")
-//   -c int
-//         [optional] count (default 1)
-//   -d
-//         debug
-//   -e string
-//         [optional] estimation (default "NO")
-//   -f int
-//         [optional] forget (default 10)
-//   -g
-//         gulp
-//   -h
-//         help
-//   -i string
-//         [required] iterator
-//   -t string
-//         [required] title
 //
+//	-a int
+//	      [required] apple
+//	-b string
+//	      [optional] bridge (default "BIG")
+//	-c int
+//	      [optional] count (default 1)
+//	-d
+//	      debug
+//	-e string
+//	      [optional] estimation (default "NO")
+//	-f int
+//	      [optional] forget (default 10)
+//	-g
+//	      gulp
+//	-h
+//	      help
+//	-i string
+//	      [required] iterator
+//	-t string
+//	      [required] title
 func main() {
 
 	flag.Parse()
 	// Required parameter
 	// - [Can Go's `flag` package print usage? - Stack Overflow](https://stackoverflow.com/questions/23725924/can-gos-flag-package-print-usage)
-	if *paramsHelp || *paramsApple == 0 || *paramsIterator == "" || *paramsTitle == "" {
+	if *paramsHelp || *paramsAdd == 0 || *paramsIncrease == "" || *paramsTitle == "" {
 		flag.Usage()
 		os.Exit(0)
 	}
 
-	fmt.Println("apple: ", *paramsApple)
-	fmt.Println("bridge: ", *paramsBridge)
-	fmt.Println("estimation: ", *paramsEstimation)
-	fmt.Println("flagship: ", *paramsFlagship)
-	fmt.Println("gulp: ", *paramsGulp)
-	fmt.Println("iterator: ", *paramsIterator)
-	fmt.Println("title: ", *paramsTitle)
-	fmt.Println("count: ", *paramsCount)
-	fmt.Println("debug: ", *paramsDebug)
+	fmt.Println("add:", *paramsAdd)
+	fmt.Println("binary:", *paramsBinary)
+	fmt.Println("environment:", *paramsEnvironment)
+	fmt.Println("filesize:", *paramsFilesize)
+	fmt.Println("global:", *paramsGlobal)
+	fmt.Println("increase:", *paramsIncrease)
+	fmt.Println("title:", *paramsTitle)
+	fmt.Println("count:", *paramsCount)
+	fmt.Println("debug:", *paramsDebug)
 }
--- flag/cmd/struct/struct.go
+++ flag/cmd/struct/struct.go
@@ -9,67 +9,60 @@ import (
 var (
 	params = struct {
 		// Required parameters
-		apple    *int
+		add      *int
 		iterator *string
 		// Optional parameters
-		flagship *int
-		bridge   *string
+		filesize *int
+		binary   *string
 		debug    *bool
 		help     *bool
 	}{
-		flag.Int("a", 0 /*          */, "[Required] apple count"),
-		flag.String("i", "" /*      */, "[Required] iterator class name"),
-		flag.Int("f", 10 /*         */, "[Optional] flagship"),
-		flag.String("b", "BIG" /*   */, "[Optional] bridge"),
-		flag.Bool("d", false /*   */, "\ndebug"),
-		flag.Bool("h", false /*   */, "\nhelp"),
+		flag.Int("a", 0 /*           */, "[Required] add"),
+		flag.String("i", "" /*       */, "[Required] iterator"),
+		flag.Int("f", 10 /*          */, "[Optional] filesize"),
+		flag.String("b", "000101" /* */, "[Optional] binary"),
+		flag.Bool("d", false /*      */, "\ndebug"),
+		flag.Bool("h", false /*      */, "\nhelp"),
 	}
 )
 
 // << Execution sample >>
 //
-// $ go run cmd/struct/struct.go -a=100 -i="iterator"
-// apple:  100
-// iterator:  iterator
-// flagship:  10
-// bridge:  BIG
-// debug:  false
+// $ go run cmd/struct/struct.go -a 100 -i "iterator"
+// add: 100
+// iterator: iterator
+// filesize: 10
+// binary: 000101
+// debug: false
 //
-// $ go run cmd/struct/struct.go -a=100 -i="iterator" -b="SMALL" -d
-// apple:  100
-// iterator:  iterator
-// flagship:  10
-// bridge:  SMALL
-// debug:  true
+// $ go run cmd/struct/struct.go -h
+// Usage of /var/folders/_q/dpw924t12bj25568xfxcd2wm0000gn/T/go-build46637023/b001/exe/struct:
 //
-// $ go run cmd/struct/struct.go -a=100 -i="iterator" -h
-// Usage of /var/folders/_q/dpw924t12bj25568xfxcd2wm0000gn/T/go-build006578447/b001/exe/struct:
-//   -a int
-//     	[Required] apple count
-//   -b string
-//     	[Optional] bridge (default "BIG")
-//   -d
-//     	[Optional] debug
-//   -f int
-//     	[Optional] flagship (default 10)
-//   -h
-//     	help
-//   -i string
-//     	[Required] iterator class name
-//
+//	-a int
+//	  	[Required] add
+//	-b string
+//	  	[Optional] binary (default "000101")
+//	-d
+//	  	debug
+//	-f int
+//	  	[Optional] filesize (default 10)
+//	-h
+//	  	help
+//	-i string
+//	  	[Required] iterator
 func main() {
 
 	flag.Parse()
 	// Required parameter
 	// - [Can Go's `flag` package print usage? - Stack Overflow](https://stackoverflow.com/questions/23725924/can-gos-flag-package-print-usage)
-	if *params.help || *params.apple == 0 || *params.iterator == "" {
+	if *params.help || *params.add == 0 || *params.iterator == "" {
 		flag.Usage()
 		os.Exit(0)
 	}
 
-	fmt.Println("apple: ", *params.apple)
-	fmt.Println("iterator: ", *params.iterator)
-	fmt.Println("flagship: ", *params.flagship)
-	fmt.Println("bridge: ", *params.bridge)
-	fmt.Println("debug: ", *params.debug)
+	fmt.Println("add:", *params.add)
+	fmt.Println("iterator:", *params.iterator)
+	fmt.Println("filesize:", *params.filesize)
+	fmt.Println("binary:", *params.binary)
+	fmt.Println("debug:", *params.debug)
 }
