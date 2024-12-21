2e67f53d83df62363d2b77ae2f6513582214208c
xshoji <xshoji@users.noreply.github.com>
2024-12-22 03:30:32 +0900

    fix

--- flag/cmd/simpleformatusage/main.go
+++ flag/cmd/simpleformatusage/main.go
@@ -1,13 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"flag"
 	"fmt"
 	"os"
+	"regexp"
+	"strings"
 )
-
-// go - Flag command line parsing in golang - Stack Overflow
-// https://stackoverflow.com/questions/19761963/flag-command-line-parsing-in-golang
 
 var (
 	// Define options and description
@@ -29,22 +29,36 @@ var (
 // Weight      :  60
 // Debug       :  false
 //
-// $ go run cmd/simple/main.go -h
-// Usage of /var/folders/_q/dpw924t12bj25568xfxcd2wm0000gn/T/go-build3551952392/b001/exe/main:
+// $ go run cmd/simpleformatusage/main.go -h
+// Usage: /var/folders/_q/dpw924t12bj25568xfxcd2wm0000gn/T/go-build3001085942/b001/exe/main [OPTIONS]
 //
-//	-b string
-//	  	birthday (format: 1900/01/02)
-//	-d
-//	  	debug
-//	-f float
-//	  	weight (default 60)
-//	-h
-//	  	help
-//	-l int
-//	  	[req] level
-//	-n string
-//	  	[req] name
+// Description:
+//   Command description is here.
+//
+// Options:
+//   -b string  birthday (format: 1900/01/02)
+//   -d         debug
+//   -f float   weight (default 60)
+//   -h         help
+//   -l int     [req] level
+//   -n string  [req] name
+
 func main() {
+
+	// Set formatted usage
+	description := "Command description is here."
+	b := new(bytes.Buffer)
+	func() { flag.CommandLine.SetOutput(b); flag.Usage(); flag.CommandLine.SetOutput(os.Stderr) }()
+	usageLines := strings.Split(b.String(), "\n")
+	usage := strings.Replace(strings.Replace(usageLines[0], ":", " [OPTIONS]", -1), " of ", ": ", -1) + "\n\nDescription:\n  " + description + "\n\nOptions:\n"
+	re := regexp.MustCompile(" +(-\\S+)( *\\S*|\t)*\n(\\s+)(.*)\n")
+	usage += re.ReplaceAllStringFunc(strings.Join(usageLines[1:], "\n"), func(m string) string {
+		parts := re.FindStringSubmatch(m)
+		return fmt.Sprintf("  %-10s %s\n", parts[1]+" "+strings.TrimSpace(parts[2]), parts[4])
+	})
+	flag.Usage = func() {
+		_, _ = fmt.Fprintf(flag.CommandLine.Output(), usage)
+	}
 
 	flag.Parse()
 	// Required parameter
