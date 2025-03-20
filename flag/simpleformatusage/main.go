7003d0eb0536d07251997b4f7a903fbece7fa709
xshoji <xshoji@users.noreply.github.com>
2025-03-21 02:19:56 +0900

    fix

--- flag/simpleformatusage/main.go
+++ flag/simpleformatusage/main.go
@@ -6,8 +6,6 @@ import (
 	"fmt"
 	"os"
 	"regexp"
-	"sort"
-	"strconv"
 	"strings"
 )
 
@@ -18,47 +16,53 @@ const (
 var (
 	// Command options ( the -h, --help option is defined by default in the flag package )
 	commandDescription      = "Here is the command description."
-	commandOptionFieldWidth = 12
-	optionLevel             = flag.Int("l" /*     */, 0 /*       */, UsageRequiredPrefix+"level")
-	optionName              = flag.String("n" /*  */, "" /*      */, UsageRequiredPrefix+"name")
-	optionBirthday          = flag.String("b" /*  */, "" /*      */, "birthday (format: 1900/01/02)")
-	optionWeight            = flag.Float64("f" /* */, 60.0 /*    */, "weight")
-	optionDebug             = flag.Bool("d" /*    */, false /*   */, "debug")
+	commandOptionFieldWidth = "12" // recommended width = general: 12, bool only: 5
+	optionLevel             = flag.Int("l" /*     */, 0 /*       */, UsageRequiredPrefix+"Level")
+	optionName              = flag.String("n" /*  */, "" /*      */, UsageRequiredPrefix+"Name")
+	optionBirthday          = flag.String("b" /*  */, "" /*      */, "Birthday (format: 1900/01/02)")
+	optionWeight            = flag.Float64("f" /* */, 60.0 /*    */, "Weight")
+	optionDebug             = flag.Bool("d" /*    */, false /*   */, "Debug")
 )
-
-// Set formatted usage
-func init() {
-	formatUsage(commandDescription, commandOptionFieldWidth)
-}
 
 // << Execution sample >>
 //
-// $ go run cmd/simpleformatusage/main.go -l 2 -n John -d
-// [ Command options ]
-// -b           birthday (format: 1900/01/02)
-// -d true      debug
-// -f 60        weight
-// -l 2         [req] level
-// -n John      [req] name
-//
-// $ go run cmd/simpleformatusage/main.go -h
-// Usage: /var/folders/_q/dpw924t12bj25568xfxcd2wm0000gn/T/go-build3001085942/b001/exe/main [OPTIONS] [-h, --help]
+// $ go run flag/simpleformatusage/main.go -h
+// Usage of /var/folders/_q/dpw924t12bj25568xfxcd2wm0000gn/T/go-build2691711322/b001/exe/main [OPTIONS] [-h, --help]
 //
 // Description:
 //   Here is the command description.
 //
 // Options:
-//   -l int       [RQD] level
-//   -n string    [RQD] name
-//   -b string    birthday (format: 1900/01/02)
-//   -d           debug
-//   -f float     weight (default 60)
+//   -b string    Birthday (format: 1900/01/02)
+//   -d           Debug
+//   -f float     Weight (default 60)
+//   -l int       [RQD] Level
+//   -n string    [RQD] Name
+//
+// $ $ go run flag/simpleformatusage/main.go -l 10 -n John
+// [ Command options ]
+//   -b         Birthday (format: 1900/01/02)
+//   -d false   Debug
+//   -f 60      Weight
+//   -l 10      [RQD] Level
+//   -n John    [RQD] Name
+//
 
 func main() {
 
+	// Format usage
+	b := new(bytes.Buffer)
+	func() { flag.CommandLine.SetOutput(b); flag.Usage(); flag.CommandLine.SetOutput(os.Stderr) }()
+	usage := strings.Replace(b.String(), ":", " [OPTIONS] [-h, --help]\n\nDescription:\n  "+commandDescription+"\n\nOptions:\n", 1)
+	re := regexp.MustCompile(`[^,] +(-\S+)(?: (\S+))?\n*(\s+)(.*)\n`)
+	flag.Usage = func() {
+		_, _ = fmt.Fprint(flag.CommandLine.Output(), re.ReplaceAllStringFunc(usage, func(m string) string {
+			return fmt.Sprintf("  %-"+commandOptionFieldWidth+"s %s\n", re.FindStringSubmatch(m)[1]+" "+strings.TrimSpace(re.FindStringSubmatch(m)[2]), re.FindStringSubmatch(m)[4])
+		}))
+	}
+
 	flag.Parse()
 	// Required parameter
-	// - [Can Go's `flag` package print usage? - Stack Overflow](https://stackoverflow.com/questions/23725924/can-gos-flag-package-print-usage)
 	if *optionLevel == 0 || *optionName == "" {
 		fmt.Printf("\n[ERROR] Missing required option\n\n")
 		flag.Usage()
@@ -70,20 +74,4 @@ func main() {
 	flag.VisitAll(func(a *flag.Flag) {
 		fmt.Printf("  -%-9s %s\n", fmt.Sprintf("%s %v", a.Name, a.Value), strings.Trim(a.Usage, "\n"))
 	})
-}
-
-// formatUsage optionFieldWidth [ general: 12, bool only: 5 ]
-func formatUsage(description string, optionFieldWidth int) {
-	b := new(bytes.Buffer)
-	func() { flag.CommandLine.SetOutput(b); flag.Usage(); flag.CommandLine.SetOutput(os.Stderr) }()
-	usageLines := strings.Split(b.String(), "\n")
-	usageFirst := strings.Replace(strings.Replace(usageLines[0], ":", " [OPTIONS] [-h, --help]", -1), " of ", ": ", -1) + "\n\nDescription:\n  " + description + "\n\nOptions:\n"
-	re := regexp.MustCompile(` +(-\S+)(?: (\S+))?\n*(\s+)(.*)\n`)
-	usageOptions := strings.Split(re.ReplaceAllStringFunc(strings.Join(usageLines[1:], "\n"), func(m string) string {
-		return fmt.Sprintf("  %-"+strconv.Itoa(optionFieldWidth)+"s %s\n", re.FindStringSubmatch(m)[1]+" "+strings.TrimSpace(re.FindStringSubmatch(m)[2]), re.FindStringSubmatch(m)[4])
-	}), "\n")
-	sort.SliceStable(usageOptions, func(i, j int) bool {
-		return strings.Count(usageOptions[i], UsageRequiredPrefix) > strings.Count(usageOptions[j], UsageRequiredPrefix)
-	})
-	flag.Usage = func() { _, _ = fmt.Fprint(flag.CommandLine.Output(), usageFirst+strings.Join(usageOptions, "\n")) }
 }
