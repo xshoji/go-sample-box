af98edff008e8e89b06563d6c39452a36c0589bf
xshoji <40511798+xshoji@users.noreply.github.com>
2025-11-24 03:35:30 +0900

    feat(cli/richtemplate): improve usage output and flag default handling for rich template CLI
    Introduce customUsage to replace old formatUsage and produce clearer usage output.
    
    Use flag.VisitAll to collect and format options with name, type, and description.
    
    Show default values for flags (string, int, float64, bool) in option usage.
    
    Remove unused imports (bytes, regexp) and adjust imports accordingly.
    
    Set flag.Usage in init to use customUsage and include command description and configured field width.

--- cli/cmd/richtemplate/main.go
+++ cli/cmd/richtemplate/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"cmp"
 	_ "embed"
 	"encoding/json"
@@ -10,7 +9,6 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"regexp"
 	"sort"
 	"strconv"
 	"strings"
@@ -27,9 +25,9 @@ var (
 	//go:embed main.go
 	srcBytes []byte
 
-	// Command options ( the -h, --help option is defined by default in the flag package )
-	commandDescription     = "Here is the command description."
-	commandOptionMaxLength = 0
+	// Command options (the -h and --help flags are provided by default in the flag package)	commandDescription     = "Here is the command description."
+	commandDescription     = "A sample command demonstrating rich template usage in Go CLI applications."
+	commandOptionMaxLength = "18"
 	optionFilePath         = defineFlagValue("f", "file-path" /*  */, UsageRequiredPrefix+"File path" /* */, "").(*string)
 	optionUrl              = defineFlagValue("u", "url" /*        */, "URL" /*                           */, "https://httpbin.org/get").(*string)
 	optionLineIndex        = defineFlagValue("l", "line-index" /* */, "Index of line" /*                 */, 10).(*int)
@@ -52,7 +50,8 @@ var (
 )
 
 func init() {
-	formatUsage(commandDescription, &commandOptionMaxLength, new(bytes.Buffer))
+	// Customize the usage message
+	flag.Usage = customUsage(os.Stdout, os.Args[0], commandDescription, commandOptionMaxLength)
 }
 
 // Build:
@@ -175,40 +174,58 @@ func handleError(err error, prefixErrMessage string) {
 // Helper function for flag
 func defineFlagValue(short, long, description string, defaultValue any) (f any) {
 	flagUsage := short + UsageDummy + description
+	defaultValueDescription := ""
 	switch v := defaultValue.(type) {
-	case string:
-		f = flag.String(short, "", UsageDummy)
-		flag.StringVar(f.(*string), long, v, flagUsage)
-	case int:
-		f = flag.Int(short, 0, UsageDummy)
-		flag.IntVar(f.(*int), long, v, flagUsage)
 	case bool:
 		f = flag.Bool(short, false, UsageDummy)
 		flag.BoolVar(f.(*bool), long, v, flagUsage)
+	case string:
+		var d string
+		if d != defaultValue.(string) {
+			defaultValueDescription = fmt.Sprintf(" (default %s)", defaultValue.(string))
+		}
+		f = flag.String(short, "", UsageDummy)
+		flag.StringVar(f.(*string), long, v, flagUsage+defaultValueDescription)
+	case int:
+		var d int
+		if d != defaultValue.(int) {
+			defaultValueDescription = fmt.Sprintf(" (default %d)", defaultValue.(int))
+		}
+		f = flag.Int(short, 0, UsageDummy)
+		flag.IntVar(f.(*int), long, v, flagUsage+defaultValueDescription)
 	case float64:
+		var d float64
+		if d != defaultValue.(float64) {
+			defaultValueDescription = fmt.Sprintf(" (default %f)", defaultValue.(float64))
+		}
 		f = flag.Float64(short, 0.0, UsageDummy)
-		flag.Float64Var(f.(*float64), long, v, flagUsage)
+		flag.Float64Var(f.(*float64), long, v, flagUsage+defaultValueDescription)
 	default:
 		panic("unsupported flag type")
 	}
 	return
 }
 
-func formatUsage(description string, maxLength *int, buffer *bytes.Buffer) {
-	func() { flag.CommandLine.SetOutput(buffer); flag.Usage(); flag.CommandLine.SetOutput(os.Stderr) }()
-	usageOption := regexp.MustCompile("(-\\S+)( *\\S*)+\n*\\s+"+UsageDummy+"\n\\s*").ReplaceAllString(buffer.String(), "")
-	re := regexp.MustCompile("\\s(-\\S+)( *\\S*)( *\\S*)+\n\\s+(.+)")
-	usageFirst := strings.Replace(strings.Replace(strings.Split(usageOption, "\n")[0], ":", " [OPTIONS] [-h, --help]", -1), " of ", ": ", -1) + "\n\nDescription:\n  " + description + "\n\nOptions:\n"
-	usageOptions := re.FindAllString(usageOption, -1)
-	for _, v := range usageOptions {
-		*maxLength = max(*maxLength, len(re.ReplaceAllString(v, " -$1")+re.ReplaceAllString(v, "$2"))+2)
+func customUsage(output io.Writer, cmdName, description, fieldWidth string) func() {
+	return func() {
+		fmt.Fprintf(output, "Usage: %s [OPTIONS] [-h, --help]\n\n", cmdName)
+		fmt.Fprintf(output, "Description:\n  %s\n\n", description)
+		fmt.Fprintln(output, "Options:")
+
+		optionUsages := make([]string, 0)
+		flag.VisitAll(func(f *flag.Flag) {
+			if f.Usage == UsageDummy {
+				return
+			}
+			valueType := strings.Replace(strings.Replace(fmt.Sprintf("%T", f.Value), "*flag.", "", -1), "Value", "", -1)
+			format := "  -%s, --%-" + fieldWidth + "s %s\n"
+			short := strings.Split(f.Usage, UsageDummy)[0]
+			mainUsage := strings.Split(f.Usage, UsageDummy)[1]
+			optionUsages = append(optionUsages, fmt.Sprintf(format, short, f.Name+" "+valueType, mainUsage))
+		})
+		sort.SliceStable(optionUsages, func(i, j int) bool {
+			return strings.Count(optionUsages[i], UsageRequiredPrefix) > strings.Count(optionUsages[j], UsageRequiredPrefix)
+		})
+		fmt.Fprint(output, strings.Join(optionUsages, ""))
 	}
-	usageOptionsRep := make([]string, 0)
-	for _, v := range usageOptions {
-		usageOptionsRep = append(usageOptionsRep, fmt.Sprintf("  -%-1s,%-"+strconv.Itoa(*maxLength)+"s%s", strings.Split(re.ReplaceAllString(v, "$4"), UsageDummy)[0], re.ReplaceAllString(v, " -$1")+re.ReplaceAllString(v, "$2"), strings.Split(re.ReplaceAllString(v, "$4"), UsageDummy)[1]+"\n"))
-	}
-	sort.SliceStable(usageOptionsRep, func(i, j int) bool {
-		return strings.Count(usageOptionsRep[i], UsageRequiredPrefix) > strings.Count(usageOptionsRep[j], UsageRequiredPrefix)
-	})
-	flag.Usage = func() { _, _ = fmt.Fprint(flag.CommandLine.Output(), usageFirst+strings.Join(usageOptionsRep, "")) }
 }
